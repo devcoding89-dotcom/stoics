7dcca284891aca8326af12b29d1d9d2d146f9f0f
akhaleedusman <engr.khaleedusman@gmail.com>
2025-11-05 23:06:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2826447618-94555",
+  "appId": "1:1051019246629:web:ea4679ee2512c93ee613cc",
+  "apiKey": "AIzaSyAA6Fi2dcknpQByikSkW1gl0xggNAb3kWA",
+  "authDomain": "studio-2826447618-94555.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1051019246629"
+};
