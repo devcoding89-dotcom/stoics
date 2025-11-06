d6324534b2f697344d0429f038ecc095e29a3e50
akhaleedusman <engr.khaleedusman@gmail.com>
2025-11-06 13:34:15 +0000

    Ohhhhthe logo not the text

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
-  const logoUrl = `https://storage.googleapis.com/stedi-assets/logos/stoics-logo-final.png`;
+  const logoUrl = `https://storage.googleapis.com/stedi-assets/logos/stoics-logo-final-v3.png`;
   return (
     <Image
       src={logoUrl}
